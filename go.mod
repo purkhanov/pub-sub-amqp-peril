4dac69e72d042b7fb05f6d122175d98a706e53ff
Nurlan Purkhanov <ppurkhanov@gmail.com>
2025-06-01 18:34:51 +0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/purkhanov/pub-sub-starter
+
+go 1.24.3
+
+require github.com/rabbitmq/amqp091-go v1.10.0
---
+++ internal/gamelogic/logs.go
@@ -0,0 +1,33 @@
+package gamelogic
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/purkhanov/pub-sub-starter/internal/routing"
+)
+
+const (
+	logsFile         = "game.log"
+	writeToDiskSleep = 1 * time.Second
+)
+
+func WriteLog(gamelog routing.GameLog) error {
+	log.Printf("received game log...")
+	time.Sleep(writeToDiskSleep)
+
+	f, err := os.OpenFile(logsFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("could not open logs file: %V", err)
+	}
+	defer f.Close()
+
+	str := fmt.Sprintf("%v %v: %v\n", gamelog.CurrentTime.Format(time.RFC3339), gamelog.Username, gamelog.Message)
+	_, err = f.WriteString(str)
+	if err != nil {
+		return fmt.Errorf("could not write to logs file: %v", err)
+	}
+	return nil
+}
