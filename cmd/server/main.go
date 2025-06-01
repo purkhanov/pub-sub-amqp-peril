f493cbb2cd1280ea128ad67911ba43d5b2e6e481
Nurlan Purkhanov <ppurkhanov@gmail.com>
2025-06-01 19:17:11 +0500

    created publish channel

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
+	"github.com/purkhanov/pub-sub-starter/internal/pubsub"
+	"github.com/purkhanov/pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -18,9 +18,22 @@ func main() {
 	}
 	defer conn.Close()
 
-	fmt.Println("Peril game server connected to RabbitMQ!")
+	publishCh, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("could not create channel: %v", err)
+	}
 
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
+	err = pubsub.PublishJSON(
+		publishCh,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{
+			IsPaused: true,
+		},
+	)
+	if err != nil {
+		log.Printf("could not publish time: %v", err)
+	}
+
+	fmt.Println("Pause message sent!")
 }
